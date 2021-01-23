fd337aec41365f1b173646ebc368f72e98131388
sylvansign <jake.d.bray@gmail.com>
2021-01-23 16:55:15 -0500

    looking good but doesn't scale (TODO)

--- src/HexGrid.js
+++ src/HexGrid.js
@@ -1,24 +1,29 @@
 
 import * as Honeycomb from 'honeycomb-grid';
 
+function coordNumToLetter(num) {
+  return String.fromCharCode(65 + num);
+}
+
 export default function HexGrid() {
-  const Hex = Honeycomb.extendHex({ size: 30 });
+  const Hex = Honeycomb.extendHex({ orientation: 'flat', size: 30, });
   const Grid = Honeycomb.defineGrid(Hex);
   // get the corners of a hex (they're the same for all hexes created with the same Hex factory)
   const corners = Hex().corners();
   // an SVG symbol can be reused;
 
   // render 10,000 hexes;
-  const hexSVGs = Grid.rectangle({ width: 10, height: 10 }).map(hex => {
+  const hexSVGs = Grid.rectangle({ width: 22, height: 14 }).map(hex => {
     const { x, y } = hex.toPoint();
     const key = `${x}-${y}`;
     const coords = hex.cartesian();
-    const coordText = `${coords.x},${coords.y}`;
+    const coordText = `${coordNumToLetter(coords.x)}${String(coords.y + 1).padStart(2, 0)}`;
+    // const coordText = `${coordNumToLetter(coords.x)}${coords.y + 1}`;
     // use hexSymbol and set its position for each hex;
     return (
       <>
         <use key={key} xlinkHref='#hex' transform={`translate(${x} ${y})`} onClick={() => console.log('TODO')} />
-        <text x={x + 15} y={y + 30} class="small">{coordText}</text>
+        <text x={x + 15} y={y + 30} class="hex-text">{coordText}</text>
       </>
     )
   });
