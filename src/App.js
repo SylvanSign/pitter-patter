18b8d6a6f1c3ab826cc8795fef668d99fcd9eeb3
sylvansign <jake.d.bray@gmail.com>
2021-01-23 16:01:55 -0500

    wip on hex grid pattern

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Honeycomb from 'honeycomb-grid';
 
 function App() {
+  const Hex = Honeycomb.extendHex({ size: 20 });
+  const Grid = Honeycomb.defineGrid(Hex);
+  // get the corners of a hex (they're the same for all hexes created with the same Hex factory)
+  const corners = Hex().corners();
+  // an SVG symbol can be reused;
+
+  // render 10,000 hexes;
+  const hexSVGs = Grid.rectangle({ width: 10, height: 10 }).map(hex => {
+    const { x, y } = hex.toPoint();
+    // use hexSymbol and set its position for each hex;
+    return <use key={`${x}-${y}`} {...{ 'xlinkHref': '#hex' }} transform={`translate(${x} ${y})`} />
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width='100%' height='100%'>
+      <symbol id='hex'>
+        <polygon points={corners.map(({ x, y }) => `${x},${y}`).join(' ')} fill='none' stroke='#999' strokeWidth='2' />
+      </symbol>
+      {hexSVGs}
+    </svg>
   );
 }
 
