42b33e2c9753ba0bf4c1f433e99f93fe82e8a174
sylvansign <jake.d.bray@gmail.com>
2021-01-23 23:57:02 -0500

    looking quite like the game

--- src/HexGrid.js
+++ src/HexGrid.js
@@ -11,16 +11,16 @@ function Hexy({ coordText, x, y }) {
   switch (Map[coordText]) {
     case HEX.silent:
       return (
-        <g class="silent">
+        <g class='silent'>
           <use xlinkHref='#hex' transform={`translate(${x} ${y})`} />
-          <text x={x + 15} y={y + 30} class="hex-text">{coordText}</text>
+          <text x={x + 15} y={y + 30} class='hex-text'>{coordText}</text>
         </g>
       );
     case HEX.danger:
       return (
-        <g class="danger">
+        <g class='danger'>
           <use xlinkHref='#hex' transform={`translate(${x} ${y})`} />
-          <text x={x + 15} y={y + 30} class="hex-text">{coordText}</text>
+          <text x={x + 15} y={y + 30} class='hex-text'>{coordText}</text>
         </g>
       );
     case HEX.human:
@@ -54,9 +54,13 @@ export default function HexGrid() {
   return (
     <svg>
       <symbol id='hex'>
-        <polygon points={corners.map(({ x, y }) => `${x},${y}`).join(' ')} stroke='black' strokeWidth='3' />
+        <polygon points={corners.map(({ x, y }) => `${x},${y}`).join(' ')} stroke='grey' strokeWidth='3' />
       </symbol>
-      <rect width="100%" height="100%" fill="black" />
+      <pattern id="stripes" width="10" height="10" patternTransform="rotate(-50 0 0)" patternUnits="userSpaceOnUse">
+        <line x1="0" y1="0" x2="0" y2="10" stroke='grey' strokeWidth='0.5' />
+      </pattern>
+      <rect width='100%' height='100%' fill='white' />
+      <rect width='100%' height='100%' fill='url(#stripes)' />
       {hexSVGs}
     </svg>
   );
