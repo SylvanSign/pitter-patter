da1bedeab138ba850ceda4d95cb6361c7ff0e41c
SylvanSign <jake.d.bray@gmail.com>
2022-01-23 14:50:43 -0500

    basic jekbox name screen

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,39 @@
-export default function Home() {
-    return (
-        <h1>
+import { useRef, useState } from "react"
 
-        </h1>
-    )
+export default function Home() {
+    const [name, setName] = useState(sessionStorage.getItem('name'))
+    const nameRef = useRef()
+
+    if (name) {
+        const onClickNameChange = (e) => {
+            e.preventDefault()
+            setName(null)
+        }
+        return (
+            <>
+                <h2>Welcome, {name} <sup>(<a href='' onClick={onClickNameChange}>change</a>)</sup></h2>
+                <button>JOIN GAME</button> <button className="button-outline">NEW GAME</button>
+            </>
+        )
+    } else {
+        const onSubmit = (e) => {
+            e.preventDefault()
+            const value = nameRef.current.value.toUpperCase()
+            setName(value)
+            sessionStorage.setItem('name', value)
+        }
+        return (
+            <>
+                <h2>Enter a name</h2>
+                <form onSubmit={onSubmit}>
+                    <input autoCapitalize="none" autoComplete="off" autoCorrect="off" id="form_name"
+                        maxLength="20" name="form[name]" pattern="^.*[^ ].*$" placeholder="ENTER YOUR NAME"
+                        style={{ textTransform: 'uppercase' }} title="Name must be at least 1 character"
+                        type="text" autoFocus={true} required={true}
+                        ref={nameRef} />
+                    <button type="submit">SUBMIT</button>
+                </form>
+            </>
+        )
+    }
 }
