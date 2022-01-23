30214e618afa6f545dca5431d5f7d776a8db3bb0
SylvanSign <jake.d.bray@gmail.com>
2022-01-23 14:50:43 -0500

    basic jekbox name screen

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import './index.css' // TODO re-add this?
+import './index.css'
 import App from './App'
+import Home from './Home'
 // enable Immer MapSet support for reachable hex stuff
 import { enableMapSet } from "immer"
 enableMapSet()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 )
