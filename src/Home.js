25333856e196aa80223f5425d79c769e7030d4ea
SylvanSign <jake.d.bray@gmail.com>
2022-01-25 21:10:34 -0500

    experimental update everything

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,39 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import io from 'socket.io-client'
 
 export default function Home() {
     const [name, setName] = useState(sessionStorage.getItem('name'))
     const nameRef = useRef()
 
+    useEffect(() => {
+
+    }, [])
+
     if (name) {
-        const onClickNameChange = (e) => {
+        const onClickNameChange = e => {
             e.preventDefault()
             setName(null)
         }
+
+        const onClickJoinGame = e => {
+            alert('TODO')
+        }
+
+        const onClickNewGame = e => {
+            alert('TODO')
+            let socket = new io({ upgrade: false, transports: ['websocket'], reconnection: true, forceNew: false });
+        }
+
         return (
             <>
-                <h2>Welcome, {name} <sup>(<a href='' onClick={onClickNameChange}>change</a>)</sup></h2>
-                <button>JOIN GAME</button> <button className="button-outline">NEW GAME</button>
+                <h2>Welcome, {name} <sup>(<a href='/' onClick={onClickNameChange}>change</a>)</sup></h2>
+                <button onClick={onClickJoinGame}>JOIN GAME</button>
+                {' '}
+                <button className="button-outline" onClick={onClickNewGame}>NEW GAME</button>
             </>
         )
     } else {
-        const onSubmit = (e) => {
+        const onSubmit = e => {
             e.preventDefault()
             const value = nameRef.current.value.toUpperCase()
             setName(value)
