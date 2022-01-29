9b2d0f38d75d89bfd7ec79041ad0c229b8fc4d6d
sylvansign <jake.d.bray@gmail.com>
2022-01-28 22:58:14 -0500

    fix a lot of linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from "react-router-dom"
 import './index.css'
-import App from './App'
 import Home from './Home'
 // enable Immer MapSet support for reachable hex stuff
 import { enableMapSet } from "immer"
@@ -11,7 +9,6 @@ enableMapSet()
 ReactDOM.render(
   <React.StrictMode>
     <Home />
-    {/* <App />  */}
   </React.StrictMode>,
   document.getElementById('root')
 )
