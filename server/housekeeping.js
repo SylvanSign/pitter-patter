0ca085a9aa0c6ca13b691fca84e63542d55ae7ff
SylvanSign <jake.d.bray@gmail.com>
2022-01-30 12:31:04 -0500

    wip on housekeeping testing and room cleaning and all that

--- server/housekeeping.js
+++ server/housekeeping.js
@@ -2,19 +2,21 @@ export default function housekeeping(db) {
   // Define clean-up method.
   const day = 24 * 60 * 60 * 1000;
   const cleanStaleMatches = async () => {
+    console.log('Housekeeping...')
     const dayAgo = Date.now() - 1 * day;
     // Retrieve matchIDs for matches unchanged for > 1 week.
     const staleMatchIDs = await db.listMatches({
       where: {
-        updatedBefore: dayAgo,
+        updatedBefore: Date.now(), //TODO should be dayAgo
       },
     });
     // Delete matches one-by-one. Could also use a queue or parallel system here.
     for (const id of staleMatchIDs) {
       await db.wipe(id);
+      console.log(`Wiped matchID ${id}`)
     }
   }
 
   // Schedule clean-up.
-  setInterval(cleanStaleMatches, day);
+  setInterval(cleanStaleMatches, 20_000); // TODO should be day
 }
