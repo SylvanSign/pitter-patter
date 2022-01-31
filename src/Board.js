3181c29342ab3c218a54cd4d1909d3088cab07b7
sylvansign <jake.d.bray@gmail.com>
2022-01-30 23:01:47 -0500

    able to actually play different maps

--- src/Board.js
+++ src/Board.js
@@ -1,14 +1,14 @@
 import Map from './Map'
 import RoundDisplay from './RoundDisplay'
 import ClueDisplay from './ClueDisplay'
-import { gridData } from './Game'
+import gridGenerator from './maps/gridGenerator'
 
 export default function Game(props) { // props from boardgame.io/react
   return (
     <div>
       {/* TODO add MapSelector somehow? */}
       <RoundDisplay {...props} />
-      <Map {...props} {...gridData} />
+      <Map {...props} {...gridGenerator(props.G.map)} />
       <ClueDisplay {...props} />
     </div >
   )
