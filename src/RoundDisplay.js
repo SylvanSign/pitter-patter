ab9f6344be609ce96adc18c98346b49262aa4084
SylvanSign <jake.d.bray@gmail.com>
2022-02-04 15:44:10 -0500

    add role at top

--- src/RoundDisplay.js
+++ src/RoundDisplay.js
@@ -1,8 +1,11 @@
-export default function RoundDisplay({ ctx: { gameover }, G: { round, }, matchData, }) {
+export default function RoundDisplay({ ctx: { gameover }, G: { round, players }, matchData, playerID }) {
+  const role = players[playerID].role
+  const youAre = `You are ${role}`
+
   const text =
     gameover
       ? `Game Over! ${gameoverText(gameover.winner, matchData)}`
-      : `Round ${round}`
+      : `${youAre} - Round ${round}`
   return (
     <h2 className="centered">{text}</h2>
   )
