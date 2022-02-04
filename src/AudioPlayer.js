ee03575911964be83b56fd4d67c5933b3ef0f0f7
SylvanSign <jake.d.bray@gmail.com>
2022-02-04 14:13:53 -0500

    continue AudioPlayer fun work

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -27,20 +27,38 @@ const EVENT_SOUNDS = {
   end: [],
   silent: [
     'public/audio/all-you-hear-is-your-heartbeat-is-it-normally-that-fast.mp3',
+    'public/audio/silence-in-all-sectors.mp3',
+    'public/audio/must-have-just-been-the-wind.mp3',
+    'public/audio/you-hear-nothing.mp3',
   ],
   noise: [
     'public/audio/did-you-hear-that.mp3',
+    'public/audio/i-thought-we-were-trying-to-be-sneaky.mp3',
+    'public/audio/noise-in-sector.mp3',
+    'public/audio/quiet-as-a-mouse.mp3',
+    'public/audio/somebody-was-a-little-clumsy.mp3',
+    'public/audio/that-one-sounded-like-it-hurt.mp3',
+    'public/audio/what-was-that-noise.mp3',
+    'public/audio/who-could-that-be.mp3',
   ],
-  attack: [],
+  attack: [
+    'public/audio/sector-under-attack.mp3',
+  ],
   kill: [
     'public/audio/a-player-has-been-eaten-which-is-the-opposite-of-what-they-were-told-to-do.mp3',
     'public/audio/a-player-has-been-killed.mp3',
     'public/audio/anybody-ever-tell-you-you-look-dead-man.mp3',
+    'public/audio/not-that-bad-to-be-eaten-by-alien.mp3',
   ],
   escape: [
     'public/audio/escape-pod-launched.mp3',
+    'public/audio/i-wish-i-could-leave-this-place-too.mp3',
   ],
-  escapeFail: [],
+  escapeFail: [
+    'public/audio/escape-pod-malfunctioned.mp3',
+    'public/audio/how-did-it-feel-dud-escape-pod.mp3',
+    'public/audio/i-wish-i-could-leave-this-place-too.mp3',
+  ],
 }
 
 export function randSourceFor(event) {
